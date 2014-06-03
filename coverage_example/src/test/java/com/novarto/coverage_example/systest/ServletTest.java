e946ec00f9840013272b37708dfdbf87e650360e
Dimitar Georgiev <dimitar.georgiev@novarto.com>
2014-06-03 17:42:56 +0300

    make sure 200 ok is asserted

--- coverage_example/src/test/java/com/novarto/coverage_example/systest/ServletTest.java
+++ coverage_example/src/test/java/com/novarto/coverage_example/systest/ServletTest.java
@@ -2,14 +2,19 @@ package com.novarto.coverage_example.systest;
 
 import java.io.IOException;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ServletTest {
 	
 	@Test
 	public void fooServlet() throws ClientProtocolException, IOException {
-		System.out.println(Request.Get("http://localhost:8080/foo").execute().returnContent().asString());
+		Response resp = Request.Get("http://localhost:8080/foo").execute();
+		HttpResponse httpResp = resp.returnResponse();
+		Assert.assertEquals(200, httpResp.getStatusLine().getStatusCode());
 	}
 }
